f9cd79135ee3ee0a4790fc33edb259af5af904d7
Preston McMann <mcmannp@mcmaster.ca>
2024-11-20 17:37:49 -0500

    Added the border code using the objPos class

--- Project.cpp
+++ Project.cpp
@@ -5,7 +5,11 @@
 using namespace std;
 
 #define DELAY_CONST 100000
+#define Board_Length 20
+#define Board_Width 10
 
+char grid[Board_Width][Board_Length];
+char input;
 bool exitFlag;
 
 void Initialize(void);
@@ -41,21 +45,48 @@ void Initialize(void)
     MacUILib_clearScreen();
 
     exitFlag = false;
+    objPos border;
+    for (int x=0;x<Board_Width;x++){
+        for(int y=0;y<Board_Length;y++){
+            border = objPos(x,y,' ');
+            if(x==0 || x==Board_Width-1 || y==0 || y==Board_Length-1){
+                border.setObjPos(x,y,'#');
+            }
+            grid[x][y]= border.getSymbol();
+        }
+    }
 }
 
 void GetInput(void)
 {
-   
+   if(MacUILib_hasChar())
+    {
+        input = MacUILib_getChar();
+    }
 }
 
 void RunLogic(void)
 {
-    
+    if(input != 0)  // if not null character
+    {
+        switch(input)
+        {                      
+            case 27:  // exit
+                exitFlag = 1;
+                break;
+        }
+    }
 }
 
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
+    for (int x=0;x<Board_Width;x++){
+        for(int y=0;y<Board_Length;y++){
+           cout<<grid[x][y];
+        }
+        cout<<"\n";
+    }
 }
 
 void LoopDelay(void)
